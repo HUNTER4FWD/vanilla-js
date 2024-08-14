506338f17d817dbb4e064972683cbffec03a01dd
HUNTER4FWD <jgarcia@fwdcostarica.com>
2024-08-14 10:29:43 -0600

    HAWK

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,36 @@
-// Inserte el código aquí
+function clonarArreglo(arr){
+return [...arr]
+}
+
+function clonarObjeto(obj) {
+    return { ...obj }
+}
+
+function sumar(...numeros) {
+    return numeros.reduce((acumulador, numeroActual) => acumulador + numeroActual, 0)
+}
+
+function saludar(nombre = 'Desconocido') {
+    return `Hola ${nombre}`
+}
 
 
 
-// NO MODIFICAR
-export { clonarArreglo, clonarObjeto, sumar, saludar };
+
+
+//arreglo por si no sabe leer puto
+const arregloOriginal = [1, 2, 3]
+const arregloClonado = clonarArreglo(arregloOriginal)
+console.log('Arreglo clonado:', arregloClonado)
+// clonar obj
+const objetoOriginal = { nombre: 'Juan', edad: 30 }
+const objetoClonado = clonarObjeto(objetoOriginal)
+console.log('Objeto clonado:', objetoClonado)
+
+//smr 
+console.log('Suma:', sumar(1, 2, 3))
+console.log('Suma:', sumar(5, 10, 15, 20))
+
+// sld
+console.log(saludar())
+console.log(saludar('Ana'))
